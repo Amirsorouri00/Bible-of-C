8b1f84394a5c963738d5e49a66906b4bbc004a3a
Amirhossein <amirsorouri26@gmail.com>
2019-04-09 19:14:43 +0430

    This chapter maybe gets reviewd and added some more instructions on C programming and its libraries later. for now it enough.

---
+++ chapter2-Types, Operators and Expressions/type-libraries.h
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include<stdio.h>
+/* Defines a family of functions that provide tests and conversions 
+that are independent of character set. for example, the function 
+tolower(c) returns the lower case value of c if c us upper case. 
+
+similarly isdigit(c) can be used to determine if the character is 
+a digit or whatever else. */
+
+

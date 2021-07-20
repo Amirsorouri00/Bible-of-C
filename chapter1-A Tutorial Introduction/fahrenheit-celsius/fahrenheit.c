a6ab4360290b906e09785893bfba582c76d25d72
SamPro910 <79455895+SamPro910@users.noreply.github.com>
2021-07-20 14:40:52 +0200

    Thank me later
    Do some indenting....

--- chapter1-A Tutorial Introduction/fahrenheit-celsius/fahrenheit.c
+++ chapter1-A Tutorial Introduction/fahrenheit-celsius/fahrenheit.c
@@ -3,18 +3,18 @@
 for fahr = 0, 20, ..., 300 */
 int main ()
 {
-int fahr, celsius;
-int lower, upper, step;
-lower = 0;
-upper = 300;
-step = 20;
-/* lower limit of temperature table */
-/* upper limit */
-/* step size */
-fahr = lower;
-while (fahr <= upper) {
-celsius = 5 * (fahr-32) / 9;
-printf("%d\t%d\n", fahr, celsius);
-fahr = fahr + step;
-}
+  int fahr, celsius;
+  int lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  /* lower limit of temperature table */
+  /* upper limit */
+  /* step size */
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
 }
